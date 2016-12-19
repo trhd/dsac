ecba38ba74f740d28f3ef2eca640a7dbf161f4a9
Hemmo Nieminen <hemmo.nieminen@iki.fi>
2016-12-19 22:46:15 +0200

    Update the existing copyright headers and add missing ones.
    Signed-off-by: Hemmo Nieminen <hemmo.nieminen@iki.fi>

--- tst/blocking_ring_buffer.c
+++ tst/blocking_ring_buffer.c
@@ -1,20 +1,20 @@
 /**
- * dsac -- Data Structures and Algorithms for C.
  * Copyright (C) 2016 Hemmo Nieminen
  *
- * This program is free software; you can redistribute it and/or modify
+ * This file is part of dsac (Data Structures and Alorithms for C).
+ *
+ * dsac is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * dsac is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU General Public License
+ * along with dsac. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <string.h>
